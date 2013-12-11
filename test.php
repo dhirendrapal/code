b328e7a9c9fa18ae716ad678a000a49223e9fede
dhirendrapal <dhiru16pal@gmail.com>
2013-12-11 08:24:05 +0530

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$DocumentRoot = __DIR__;
-//$bfileloc = "C:/wamp/bin/mysql/mysql5.5.16/data/";
-$bfileloc = "D:/xampp/mysql/data/";
-echo $command = 'mysqlbinlog '.$bfileloc.'mysql-bin.000001 > '.$DocumentRoot.'/backup/blog1.sql';
-system($command);
-?>
